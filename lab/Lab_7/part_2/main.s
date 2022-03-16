d9522a3f8b77cdbd006a3f4c832ca2fdf366d602
athakkar2 <athakkar@stevens.edu>
2022-03-16 01:21:49 +0000

    ajay

---
+++ lab/Lab_7/part_2/main.s
@@ -0,0 +1,200 @@
+	.arch armv8-a
+	.file	"main.cc"
+	.text
+	.section	.rodata
+	.align	3
+	.type	_ZStL19piecewise_construct, %object
+	.size	_ZStL19piecewise_construct, 1
+_ZStL19piecewise_construct:
+	.zero	1
+	.local	_ZStL8__ioinit
+	.comm	_ZStL8__ioinit,1,8
+	.section	.text._ZSt4sqrtIiEN9__gnu_cxx11__enable_ifIXsrSt12__is_integerIT_E7__valueEdE6__typeES3_,"axG",@progbits,_ZSt4sqrtIiEN9__gnu_cxx11__enable_ifIXsrSt12__is_integerIT_E7__valueEdE6__typeES3_,comdat
+	.align	2
+	.weak	_ZSt4sqrtIiEN9__gnu_cxx11__enable_ifIXsrSt12__is_integerIT_E7__valueEdE6__typeES3_
+	.type	_ZSt4sqrtIiEN9__gnu_cxx11__enable_ifIXsrSt12__is_integerIT_E7__valueEdE6__typeES3_, %function
+_ZSt4sqrtIiEN9__gnu_cxx11__enable_ifIXsrSt12__is_integerIT_E7__valueEdE6__typeES3_:
+.LFB1794:
+	.cfi_startproc
+	stp	x29, x30, [sp, -32]!
+	.cfi_def_cfa_offset 32
+	.cfi_offset 29, -32
+	.cfi_offset 30, -24
+	mov	x29, sp
+	str	w0, [sp, 28]
+	ldr	w0, [sp, 28]
+	scvtf	d0, w0
+	bl	sqrt
+	ldp	x29, x30, [sp], 32
+	.cfi_restore 30
+	.cfi_restore 29
+	.cfi_def_cfa_offset 0
+	ret
+	.cfi_endproc
+.LFE1794:
+	.size	_ZSt4sqrtIiEN9__gnu_cxx11__enable_ifIXsrSt12__is_integerIT_E7__valueEdE6__typeES3_, .-_ZSt4sqrtIiEN9__gnu_cxx11__enable_ifIXsrSt12__is_integerIT_E7__valueEdE6__typeES3_
+	.text
+	.align	2
+	.global	main
+	.type	main, %function
+main:
+.LFB1793:
+	.cfi_startproc
+	stp	x29, x30, [sp, -128]!
+	.cfi_def_cfa_offset 128
+	.cfi_offset 29, -128
+	.cfi_offset 30, -120
+	mov	x29, sp
+	mov	w0, 70
+	str	w0, [sp, 124]
+	mov	w0, 70
+	bl	_ZSt4sqrtIiEN9__gnu_cxx11__enable_ifIXsrSt12__is_integerIT_E7__valueEdE6__typeES3_
+	fcvtzs	w0, d0
+	str	w0, [sp, 124]
+	add	x0, sp, 136
+	sub	x0, x0, #48
+	adrp	x1, .LC0
+	add	x1, x1, :lo12:.LC0
+	ld1	{v0.16b - v1.16b}, [x1]
+	st1	{v0.16b - v1.16b}, [x0]
+	add	x0, sp, 136
+	sub	x0, x0, #80
+	adrp	x1, .LC1
+	add	x1, x1, :lo12:.LC1
+	ld1	{v0.16b - v1.16b}, [x1]
+	st1	{v0.16b - v1.16b}, [x0]
+	add	x1, sp, 56
+	add	x0, sp, 88
+	mov	w2, 4
+	bl	_Z3dotPmS_i
+	mov	x1, x0
+	adrp	x0, :got:_ZSt4cout
+	ldr	x0, [x0, #:got_lo12:_ZSt4cout]
+	bl	_ZNSolsEm
+	mov	w1, 10
+	bl	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_c
+	mov	w0, 2
+	str	w0, [sp, 40]
+	mov	w0, 3
+	str	w0, [sp, 44]
+	mov	w0, -3
+	str	w0, [sp, 48]
+	mov	w0, 5
+	str	w0, [sp, 52]
+	mov	w0, -3
+	str	w0, [sp, 24]
+	mov	w0, 2
+	str	w0, [sp, 28]
+	mov	w0, 3
+	str	w0, [sp, 32]
+	mov	w0, 2
+	str	w0, [sp, 36]
+	add	x1, sp, 24
+	add	x0, sp, 40
+	mov	w2, 4
+	bl	_Z4dot1PiS_i
+	mov	x1, x0
+	adrp	x0, :got:_ZSt4cout
+	ldr	x0, [x0, #:got_lo12:_ZSt4cout]
+	bl	_ZNSolsEl
+	mov	w1, 10
+	bl	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_c
+	fmov	d3, 3.0e+0
+	fmov	d2, 1.0e+0
+	fmov	d1, 2.0e+0
+	fmov	d0, 1.0e+0
+	bl	_Z4quaddddd
+	adrp	x0, :got:_ZSt4cout
+	ldr	x0, [x0, #:got_lo12:_ZSt4cout]
+	bl	_ZNSolsEd
+	mov	w1, 10
+	bl	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_c
+	mov	w0, 0
+	ldp	x29, x30, [sp], 128
+	.cfi_restore 30
+	.cfi_restore 29
+	.cfi_def_cfa_offset 0
+	ret
+	.cfi_endproc
+.LFE1793:
+	.size	main, .-main
+	.section	.rodata
+	.align	3
+.LC0:
+	.xword	2
+	.xword	3
+	.xword	2
+	.xword	5
+	.align	3
+.LC1:
+	.xword	3
+	.xword	2
+	.xword	3
+	.xword	2
+	.text
+	.align	2
+	.type	_Z41__static_initialization_and_destruction_0ii, %function
+_Z41__static_initialization_and_destruction_0ii:
+.LFB2291:
+	.cfi_startproc
+	stp	x29, x30, [sp, -32]!
+	.cfi_def_cfa_offset 32
+	.cfi_offset 29, -32
+	.cfi_offset 30, -24
+	mov	x29, sp
+	str	w0, [sp, 28]
+	str	w1, [sp, 24]
+	ldr	w0, [sp, 28]
+	cmp	w0, 1
+	bne	.L7
+	ldr	w1, [sp, 24]
+	mov	w0, 65535
+	cmp	w1, w0
+	bne	.L7
+	adrp	x0, _ZStL8__ioinit
+	add	x0, x0, :lo12:_ZStL8__ioinit
+	bl	_ZNSt8ios_base4InitC1Ev
+	adrp	x0, __dso_handle
+	add	x2, x0, :lo12:__dso_handle
+	adrp	x0, _ZStL8__ioinit
+	add	x1, x0, :lo12:_ZStL8__ioinit
+	adrp	x0, :got:_ZNSt8ios_base4InitD1Ev
+	ldr	x0, [x0, #:got_lo12:_ZNSt8ios_base4InitD1Ev]
+	bl	__cxa_atexit
+.L7:
+	nop
+	ldp	x29, x30, [sp], 32
+	.cfi_restore 30
+	.cfi_restore 29
+	.cfi_def_cfa_offset 0
+	ret
+	.cfi_endproc
+.LFE2291:
+	.size	_Z41__static_initialization_and_destruction_0ii, .-_Z41__static_initialization_and_destruction_0ii
+	.align	2
+	.type	_GLOBAL__sub_I_main, %function
+_GLOBAL__sub_I_main:
+.LFB2292:
+	.cfi_startproc
+	stp	x29, x30, [sp, -16]!
+	.cfi_def_cfa_offset 16
+	.cfi_offset 29, -16
+	.cfi_offset 30, -8
+	mov	x29, sp
+	mov	w1, 65535
+	mov	w0, 1
+	bl	_Z41__static_initialization_and_destruction_0ii
+	ldp	x29, x30, [sp], 16
+	.cfi_restore 30
+	.cfi_restore 29
+	.cfi_def_cfa_offset 0
+	ret
+	.cfi_endproc
+.LFE2292:
+	.size	_GLOBAL__sub_I_main, .-_GLOBAL__sub_I_main
+	.section	.init_array,"aw"
+	.align	3
+	.xword	_GLOBAL__sub_I_main
+	.hidden	__dso_handle
+	.ident	"GCC: (Debian 10.2.1-6) 10.2.1 20210110"
+	.section	.note.GNU-stack,"",@progbits
